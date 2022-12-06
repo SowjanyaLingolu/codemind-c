6e0ef64f7dbb233dfc24284f38c64be69bc05ccc
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2022-12-06 11:41:54 +0530

    #include<stdio.h> #include<math.h> int main() {     float a,b,c,s,x;     scanf("%f%f%f",&a,&b,&c);     s=(a+b+c)/2;     x=sqrt (s*(s-a)*(s-b)*(s-c));     printf("%0.2f",x); }

---
+++ Area_of_Triangle_.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,s,x;
+    scanf("%f%f%f",&a,&b,&c);
+    s=(a+b+c)/2;
+    x=sqrt (s*(s-a)*(s-b)*(s-c));
+    printf("%0.2f",x);
+}
