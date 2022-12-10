c3d7b411b11148971923e013d2237b10db787cdc
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2022-12-10 20:15:30 +0530

    #include<stdio.h> int main() {     int a,i,rem,sum=0;     scanf("%d",&a);     for(i=1;i<a;i++)     {         rem=a%i;         if(rem==0)         {             sum += i;
            }
    
        }
        if(sum==a)
     {
         printf("True");
     } 
     else
     {
         printf("False");
     }
    }

---
+++ Perfect_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,rem,sum=0;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        rem=a%i;
+        if(rem==0)
+        {
+            sum += i;
+            
+        }
+
+    }
+    if(sum==a)
+ {
+     printf("True");
+ } 
+ else
+ {
+     printf("False");
+ }
+}
