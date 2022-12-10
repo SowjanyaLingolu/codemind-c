2482c756ced4431cbacfe1952fc55ee2a818b63f
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2022-12-10 20:53:42 +0530

    #include<stdio.h> int main() {     int n,square,rem,sum=0;     scanf("%d",&n);     square=n*n;     while(square!=0)     {     rem=square%10;     sum=sum+rem;     square=square/10;     }     if(sum==n)     {         printf("Neon Number");     }     else     {         printf("Not Neon Number");     } }

---
+++ Neon_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,square,rem,sum=0;
+    scanf("%d",&n);
+    square=n*n;
+    while(square!=0)
+    {
+    rem=square%10;
+    sum=sum+rem;
+    square=square/10;
+    }
+    if(sum==n)
+    {
+        printf("Neon Number");
+    }
+    else
+    {
+        printf("Not Neon Number");
+    }
+}
