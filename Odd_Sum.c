0f3e586455c53875eb5889ac723efa20d9a7c177
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-13 11:55:07 +0530

    #include<stdio.h> int main() {     int n,a=0,i,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for (i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             a=a+arr[i];         }     }     printf("%d",a); }

---
+++ Odd_Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,a=0,i,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            a=a+arr[i];
+        }
+    }
+    printf("%d",a);
+}
