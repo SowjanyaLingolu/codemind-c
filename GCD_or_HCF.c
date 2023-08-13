eeb58f86b446520c9834501693801cb5e76ca22b
SowjanyaLingolu <119915201+SowjanyaLingolu@users.noreply.github.com>
2023-08-13 10:33:01 +0530

    #include<stdio.h> int main() {     int i,a,b,g;     scanf("%d %d",&a,&b);     for(i=1;i<=a&&i<=b;i++)     {         if(a%i==0&&b%i==0)         g=i;     }     printf("%d",g); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,gcd,i;
-    scanf("%d%d",&a,&b);
-    for(i=1;i<=a && i<=b;i++)
+    int i,a,b,g;
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a&&i<=b;i++)
     {
-        if(a%i==0 && b%i==0)
-        {
-            gcd=i;
-        }
+        if(a%i==0&&b%i==0)
+        g=i;
     }
-    printf("%d",gcd);
+    printf("%d",g);
 }
